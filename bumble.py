cae1c7cf71e8faaa1780e4b8d041ce7550895521
Viswajith <viswajithkalathil@gmail.com>
2025-06-30 23:57:23 +0530

    Update bumble.py
    updated the code - selenium 4

--- bumble.py
+++ bumble.py
@@ -1,48 +1,35 @@
 from selenium import webdriver
 from time import sleep
-from login_details import email, password
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 
 class BumbleBot():
     def __init__(self):
-        self.driver = webdriver.Chrome('C:/Users/V/Desktop/chromedriver.exe')
+        service = Service('C:/Users/DELL/Desktop/project/chromedriver.exe')
+        self.driver = webdriver.Chrome(service=service)
 
     def login(self):
         self.driver.get('https://bumble.com/get-started')
         sleep(3)
 
-        
-        
-        
-
-        fb_btn = bot.driver.find_element_by_xpath('//*[@id="main"]/div/div[1]/div[2]/main/div/div[3]/form/div[1]/div/div[2]/button')
-        fb_btn.click()
-        sleep(4)
+    
+        security_key_btn = bot.driver.find_element(By.XPATH, '//*[@id="main"]/div/div[1]/div[2]/main/div/div[3]/form/div/div/div[1]/div')
+        security_key_btn.click()
+        sleep(45)
          #switch pages
-        base_window = self.driver.window_handles[0]
-        self.driver.switch_to_window(self.driver.window_handles[1])
+        #base_window = self.driver.window_handles[0]
+        #self.driver.switch_to_window(self.driver.window_handles[1])
         sleep(4)
-        email_in = self.driver.find_element_by_xpath('//*[@id="email"]')
-        email_in.send_keys(email)
-        sleep(2)
-        pass_in = self.driver.find_element_by_xpath('//*[@id="pass"]')
-        pass_in.send_keys(password)
-        sleep(2)
-        login_btn = self.driver.find_element_by_xpath('/html/body/div/div[2]/div[1]/form/div/div[3]/label[2]')
-        login_btn.click()
-        sleep(4)
-        self.driver.switch_to_window(base_window)
-        sleep(15)
-        
-        
-        like_btn = self.driver.find_element_by_xpath('/html/body/div/div/div[1]/main/div[2]/div/div/span/div[2]/div/div[2]/div/div[3]/div')
+                    
+        like_btn = self.driver.find_element(By.XPATH, '/html/body/div/div/div[1]/main/div[2]/div/div/span/div[2]/div/div[2]/div/div[3]/div')
 
         i=1
         while True:
             try:
                 like_btn.click()
 
-                sleep(2)
+                sleep(1.2)
                 print(f"Swiped right for {i} times")
                 i += 1
             except:
@@ -53,6 +40,5 @@ class BumbleBot():
 
 bot = BumbleBot()
 bot.login()
-
 
 
